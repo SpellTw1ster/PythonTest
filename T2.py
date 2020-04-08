c0689dffba9e366c859d6a111090094b800bf6c1
n9necage <n9necage@gmail.com>
2020-04-08 12:27:44 +0300

    git commit "first commit"

---
+++ T2.py
@@ -0,0 +1,5 @@
+print("Добро пожаловать в калькулятор! ")
+number_1 = int (input ('Введите ваше первое число: '))
+number_2 = int (input ('Введите ваше второе число: '))
+print ('{} + {} = '.format (number_1, number_2))
+print (number_1 + number_2)
